690d1ff359fa053e5587215307a32d5c525145a8
Dmitry Zubelevich <dz@jelastic.com>
2016-05-01 16:07:45 +0300

    Initial commit

---
+++ jelastic/scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Simple deploy and undeploy scenarios for JBoss7
+
+WGET=$(which wget);
+
+function _deploy(){
+     [ "x${context}" == "xroot" ] && context="ROOT";
+     [ -f "${WEBROOT}/${context}.war" ] && rm -f ${WEBROOT}/${context}.war;
+     [ -f "${WEBROOT}/${context}.war.undeployed" ] && mv ${WEBROOT}/${context}.war.undeployed ${WEBROOT}/${context}.war.deployed
+     $WGET --no-check-certificate --content-disposition -O "${WEBROOT}/${context}.war" "$package_url";
+}
+
+function _undeploy(){
+     [ "x${context}" == "xroot" ] && context="ROOT";
+     [ -f "${WEBROOT}/${context}.war" ] &&  rm -f ${WEBROOT}/${context}.war || return 0;
+}
